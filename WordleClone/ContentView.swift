a48f5a4f2053d690f18434227e18d2eeb32f90b1
Justin Grimes <grimesajustin@gmail.com>
2024-04-08 01:57:38 -0400

    Initial Commit

---
+++ WordleClone/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  WordleClone
+//
+//  Created by Justin Grimes on 4/8/24.
+//  Copyright © 2024 Justin Grimes. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WordleClone/WordleCloneApp.swift
@@ -0,0 +1,18 @@
+//
+//  WordleCloneApp.swift
+//  WordleClone
+//
+//  Created by Justin Grimes on 4/8/24.
+//  Copyright © 2024 Justin Grimes. All rights reserved.
+//
+
+import SwiftUI
+
+@main
+struct WordleCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
